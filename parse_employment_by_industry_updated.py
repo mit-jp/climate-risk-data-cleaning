4f5b86384d981054cb64f85a54230b765e5cb71d
ShelliOrzach <sorzach@mit.edu>
2021-07-12 16:11:03 -0400

    parse_employment_by_industry pt 1 fixed

--- parse_employment_by_industry_updated.py
+++ parse_employment_by_industry_updated.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import numpy as np
+from us import states
 import fix_differences as diff
 #pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 df = pd.read_csv(r'/Users/ShelliOrzach/ShelliOrzach1/Documents/DOE MATLAB Updated/CAEMP25N__ALL_AREAS_2001_2019.csv', nrows=105270, low_memory=False)
 df = pd.pivot(df, index='GeoName', columns='Description', values=['2019']).reset_index()
-print(df)
-#state_names = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
-#states = [item.lower() for item in state_names]
-#df = df[~df['GeoName'].isin(states)]
-#df = df[~df['GeoName'].str.contains('united states')]
 df['County'] = np.nan
 df['State'] = np.nan
 df[['County', 'State']] = df['GeoName'].str.rsplit(', ', n=1, expand=True)
 df = pd.concat([df.iloc[:, 34:37], df.iloc[:, 33], df.iloc[:, 1:33]], axis=1)
+df['State'] = df['State'].str.replace('*', '')
 df = df.sort_values(by=['State', 'County'])
 df = df.iloc[0:3138, :]
-#print(df)
-df.to_csv(r'/Users/ShelliOrzach/ShelliOrzach1/Documents/DOE MATLAB Updated/Cypress Output Python/Employment by industry updated.csv', index = False)
+df['State'] = df['State'].apply(lambda x: states.lookup(str(x)).fips)
+df = df.sort_values(by=['State', 'County'])
+i = 0
+for i in range(len(df)):
+    df.iloc[i, 0] = str(df.iloc[i, 0]) + '_' + str(states.lookup(df.iloc[i, 1]))
+df = df.drop(['State'], axis=1)
+df['County'] = df['County'].str.lower()
+df['County'] = df['County'].str.replace(' county', '')
+df['County'] = df['County'].str.replace(' city and', '')
+df['County'] = df['County'].str.replace(' borough', '')
+df['County'] = df['County'].str.replace(' municipality', '')
+df['County'] = df['County'].str.replace(' census area', '')
+df['County'] = df['County'].str.replace(' parish', '')
+df['County'] = df['County'].str.replace(' city', '')
+#df = diff.fix(df, 0, 2)
+#df['County'] = df['County'].apply(lambda x: x + states.lookup(str(df['State'])))
+df.to_csv(r'/Users/ShelliOrzach/ShelliOrzach1/Documents/DOE MATLAB Updated/Cypress Output Python/Employment by industry updated pt 1.csv', index = False)
