4a03438b32c1d77b21ee2be00b859eba2ff1bf21
ShelliOrzach <sorzach@mit.edu>
2021-08-04 14:01:06 -0400

    all scprits besides those with virginia issues done!!

---
+++ parse_county_no_id.py
@@ -0,0 +1,7 @@
+import pandas as pd
+pd.set_option('display.max_rows', None)
+df = pd.read_csv(r'Project data/esri counties for comparison.csv')
+df['County'] = df['County'].str.lower()
+df['County'] = df['County'].str.replace(' county', '')
+df['County'] = df['County'].str.replace(' city', '')
+df.to_csv(r'Parsed data/County no id.csv', index = False)
