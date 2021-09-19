546e9c379c20a2dc35baf8bf1baf856ae09e182a
ShelliOrzach <sorzach@mit.edu>
2021-09-19 11:59:01 -0400

    tackled the messiest fish

--- parse_employment_by_industry_updated.py
+++ parse_employment_by_industry_updated.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import re
 from us import states
 import fix_differences as diff
 #pd.set_option('display.max_rows', None)
@@ -13,6 +14,7 @@ df = pd.concat([df.iloc[:, 34:37], df.iloc[:, 33], df.iloc[:, 1:33]], axis=1)
 df['State'] = df['State'].str.replace('*', '')
 df = df.sort_values(by=['State', 'County'])
 df = df.iloc[0:3114, :]
+#print(df.iloc[:, 0:2])
 df['State'] = df['State'].apply(lambda x: states.lookup(str(x)).fips)
 df = df.sort_values(by=['State', 'County'])
 i = 0
@@ -38,27 +40,53 @@ for i in range(2, df.shape[1]):
 df.iloc[:, 1:] = df.iloc[:, 1:].astype(float).round(2)
 df = pd.concat([df[0:548], pd.DataFrame([], index=[548]), df[548:]])
 df.iloc[548,:] = df.iloc[550,:]
+df.iloc[548, 0] = 'kalawao_hawaii'
+df.iloc[550, 0] = 'maui_hawaii'
 df1=df.copy()
-df1.iloc[429,:],df1.iloc[430,:]=df.iloc[430,:],df.iloc[429,:]
-df1.iloc[643,:],df1.iloc[644,:]=df.iloc[644,:],df.iloc[643,:]
-df1.iloc[711,:],df1.iloc[712,:], df1.iloc[713,:]=df.iloc[712,:],df1.iloc[713,:],df.iloc[711,:]
-df1.iloc[740,:],df1.iloc[741,:], df1.iloc[742,:]=df.iloc[741,:],df1.iloc[742,:],df.iloc[740,:]
+#df1.iloc[429,:],df1.iloc[430,:]=df.iloc[430,:],df.iloc[429,:]
+#df1.iloc[643,:],df1.iloc[644,:]=df.iloc[644,:],df.iloc[643,:]
+#df1.iloc[711,:],df1.iloc[712,:], df1.iloc[713,:]=df.iloc[712,:],df1.iloc[713,:],df.iloc[711,:]
+df1.iloc[740,:],df1.iloc[741,:]=df.iloc[741,:],df1.iloc[740,:]
 df1.iloc[1138,:],df1.iloc[1139,:]=df.iloc[1139,:],df.iloc[1138,:]
 df1.iloc[1351,:],df1.iloc[1352,:]=df.iloc[1352,:],df.iloc[1351,:]
 df1.iloc[1432,:],df1.iloc[1433,:]=df.iloc[1433,:],df.iloc[1432,:]
-df1.iloc[2447,:],df1.iloc[2448,:]=df.iloc[2448,:],df.iloc[2447,:]
-df1.iloc[2581,:],df1.iloc[2582,:], df1.iloc[2583,:], df1.iloc[2584,:]=df.iloc[2582,:],df1.iloc[2583,:],df.iloc[2584,:], df1.iloc[2581,:]
+#df1.iloc[2447,:],df1.iloc[2448,:]=df.iloc[2448,:],df.iloc[2447,:]
+#df1.iloc[2581,:],df1.iloc[2582,:], df1.iloc[2583,:], df1.iloc[2584,:]=df.iloc[2582,:],df1.iloc[2583,:],df.iloc[2584,:], df1.iloc[2581,:]
 df = df1
 del(df1)
 
+
 ref = pd.read_csv(r'Parsed data/ID match resorted.csv')
 ref_virginia = ref.loc[ref["county_State"].str.endswith("_virginia")]
-df_virginia = df.loc[df["county_State"].str.endswith("_virginia")]
-df_virginia = df_virginia.sort_values(by = ['county_State'], axis=0)
+df_virginia = df.loc[df["county_state"].str.endswith("_virginia")]
+#print(df_virginia)
+df_virginia = df_virginia.reset_index()
+df_virginia = df_virginia.drop(['index'], axis=1)
+
+i = 0
+while i < 133:
+    df_virginia['county_state'] = df_virginia['county_state'].str.replace('\(independent\)', '')
+    df_virginia['county_state'] = df_virginia['county_state'].str.replace('+', ',')
+    m = df_virginia.iloc[i,0].split(',')
+    if len(m) == 2:
+        df_virginia.iloc[i, 0] = m[0] + '_virginia'
+        df_virginia = df_virginia.append(df_virginia.iloc[i, :], ignore_index=True)
+        df_virginia.iloc[-1, 0] = m[1]
+    elif len(m) == 3:
+        df_virginia = df_virginia.append(df_virginia.iloc[i, :], ignore_index=True)
+        df_virginia = df_virginia.append(df_virginia.iloc[i, :], ignore_index=True)
+        df_virginia.iloc[-2, 0] = m[1] + '_virginia'
+        df_virginia.iloc[-1, 0] = m[2]
+        df_virginia.iloc[i, 0] = m[0] + '_virginia'
+    i += 1
+
+df_virginia['county_state'] = df_virginia['county_state'].str.strip()
+df_virginia['county_state'] = df_virginia['county_state'].str.replace(' _', '_')
+df_virginia = df_virginia.sort_values(by = ['county_state'], axis=0)
 df_virginia[['STATEFP', 'COUNTYFP']] = [np.nan, np.nan]
 df_copy = df_virginia.copy()
 df_virginia.iloc[64,:],df_virginia.iloc[65,:], df_virginia.iloc[66,:]=df_copy.iloc[65,:],df_copy.iloc[66,:],df_copy.iloc[64,:]
-df_virginia = pd.concat([df_virginia[0:69], pd.DataFrame([], index=[69]), df_virginia[69:]])
+#df_virginia = pd.concat([df_virginia[0:69], pd.DataFrame([], index=[69]), df_virginia[69:]])
 i = 0
 while i < ref_virginia.shape[0] - 1:
     if ref_virginia.iat[i, 2] != (df_virginia.iat[i, 0]):
@@ -67,12 +95,29 @@ while i < ref_virginia.shape[0] - 1:
         print(df_virginia.iat[i, 0])
     i = i + 1
     #print(df_virginia.iloc[i, 6])
+
 i = 0
 for i in range(df_virginia.shape[0]):
     df_virginia.iloc[i, 34] = ref_virginia.iloc[i, 0]
     df_virginia.iloc[i, 35] = ref_virginia.iloc[i, 1]
-df_virginia = df_virginia.sort_values(by = ['STATEFP', 'COUNTYFP'], axis=0)
+#df_virginia = df_virginia.sort_values(by = ['STATEFP', 'COUNTYFP'], axis=0)
 df_virginia = df_virginia.drop(['STATEFP', 'COUNTYFP'], axis=1)
-df = pd.concat([df.iloc[0:2820], df_virginia, df.iloc[2952:,:]], axis=0, ignore_index=True)
-#df = diff.fix(df, 0, 2, 2)
-#df.to_csv(r'/Users/ShelliOrzach/ShelliOrzach1/Documents/DOE MATLAB Updated/Cypress Output Python/Employment by industry updated temp.csv', index = False)
+df = pd.concat([df.iloc[0:2820, :], df_virginia, df.iloc[2925:,:]], axis=0, ignore_index=True)
+
+df1=df.copy()
+df1.iloc[3069,:],df1.iloc[3070,:]=df.iloc[3070,:],df.iloc[3069,:]
+#df1.iloc[3097,:],df1.iloc[3098,:]=df.iloc[3098,:],df.iloc[3097,:]
+#df1.iloc[3124,:],df1.iloc[3125,:]=df.iloc[3125,:],df.iloc[3124,:]
+df = df1
+del(df1)
+
+df = diff.fix(df, 0, 2, 2)
+
+df = df.reset_index()
+ref= ref.reset_index()
+df = pd.concat([ref.iloc[:, 1:3], df.iloc[:, 1:]], axis=1)
+df = df.sort_values(['STATEFP', 'COUNTYFP'])
+
+df = diff.fix(df, 1, 1, 1)
+
+df.to_csv(r'Parsed data/Employment by Industry.csv', index = False)
