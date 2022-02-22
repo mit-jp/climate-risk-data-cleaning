d81a0041b5763f41c0fb81a3c29f80edc25ef72a
ShelliOrzach <sorzach@mit.edu>
2022-02-21 19:12:19 -0500

    new cdc estimation

--- parse_all_cause_mortality2.py
+++ parse_all_cause_mortality2.py
@@ -1,6 +1,7 @@
 import fix_differences as diff
 import pandas as pd
 import numpy as np
+import cdc_estimation_functions as rg
 
 # Import all datasets
 df_county = pd.read_csv(r'CDC Wonder Data/All-cause mortality at county level 2016.txt', sep='\t',
@@ -26,32 +27,18 @@ df_county['County ANSI'] = df_county['County Code'] - (df_county['County Code'] 
 df_county = df_county.drop(['County', 'County Code'], axis=1)  # drop county and dounty code rows
 df_state = df_state.drop(['State'], axis=1)
 
-'''# Group ages into specified ranges
-df_county = df_county.replace({'Age Group Code': {'1': '0-5', '1-4': '0-5', '5-9': '5-25', '10-14': '5-25',
-                                                  '15-19': '5-25', '20-24': '5-25', '25-34': '25+', '35-44': '25+',
-                                                  '45-54': '25+', '55-64': '25+', '65-74': '25+', '75-84': '25+',
-                                                  '85+': '25+'}})
-df_state = df_state.replace({'Age Group Code': {'1': '0-5', '1-4': '0-5', '5-9': '5-25', '10-14': '5-25',
-                                                '15-19': '5-25', '20-24': '5-25', '25-34': '25+', '35-44': '25+',
-                                                '45-54': '25+', '55-64': '25+', '65-74': '25+', '75-84': '25+',
-                                                '85+': '25+'}})
-df_national = df_national.replace({'Age Group Code': {'1': '0-5', '1-4': '0-5', '5-9': '5-25', '10-14': '5-25',
-                                                      '15-19': '5-25', '20-24': '5-25', '25-34': '25+',
-                                                      '35-44': '25+', '45-54': '25+', '55-64': '25+',
-                                                      '65-74': '25+', '75-84': '25+', '85+': '25+'}})'''
-
 # Clean up data and reorder
 # replace 'supprssed' and 'missing' with nans to allow for math operations
 df_county = df_county.replace(
     {'Deaths': {'Suppressed': np.nan, 'Missing': np.nan}, 'Population': {'Suppressed': np.nan, 'Missing': np.nan}})
 df_state = df_state.replace(
     {'Deaths': {'Suppressed': np.nan, 'Missing': np.nan}, 'Population': {'Suppressed': np.nan, 'Missing': np.nan}})
-
 # set class of deaths and population to float
+
+
 df_county['Deaths'] = df_county['Deaths'].astype(float)
 df_county['Population'] = df_county['Population'].astype(float)
 df_state['Deaths'] = df_state['Deaths'].astype(float)
-
 
 # sum column values by state, then county, then age group code. needs to be done because multiple values for each combination due to higher granualrity in orginial data
 df_county = df_county.groupby(['State ANSI', 'County ANSI', 'Age Group Code']).sum()
@@ -71,56 +58,97 @@ df_state = df_state.unstack(level=1)
 df_state.columns = ['_'.join(col) for col in df_state.columns.values]
 df_state = df_state.reset_index()
 
+df_national['Percent Deaths'] = df_national['Deaths']/df_national['Population']
+
+'''
+for age in ages:
+    ep = 0
+    excp = 0
+    for i in range(df_county.shape[0]):
+            if df_county.loc[i, 'Population_' + age] == 0:
+                ep += 1
+            if df_county.loc[i, 'Population_' + age] == 0 and df_county.loc[i, 'Deaths_' + age] != 0:
+                excp += 1
+    print(age + ': ' + str(ep))
+    print("\\")
+    print(age + ': ' + str(excp))
+'''
+
 for age in ages:
     df_county['Percent Deaths ' + age] = df_county['Deaths_' + age]/df_county['Population_' + age]
+    df_state[['CalDt' + age, 'CalPop' + age, 'CalPer' + age]] = [np.nan, np.nan, np.nan]
+
+i = 0
+for i in range(df_state.shape[0]):
+    lookat = df_county[df_county['State ANSI'] == df_state.loc[i, 'State Code']]  # extract a subtable for each state
+    for age in ages:
+        df_state.loc[i, 'CalDt' + age] = lookat.loc[:, 'Deaths_' + age].sum()
+        df_state.loc[i, 'CalPop' + age] = lookat[lookat['Deaths_' + age] == 0]['Population_' + age].sum()
+
+for age in ages:
+    df_state['CalPer' + age] = df_state['CalDt' + age]/df_state['CalPop' + age][~(df_state['CalPop' + age] == 0)]
+    i = 0
+    # loop through all counties in county table
+    for i in range(df_county.shape[0]):
+        if df_county.loc[i, 'Deaths_' + age] == 0:
+            state = df_state[df_state['State Code'] == df_county.loc[i, 'State ANSI']].reset_index()
+            if state.loc[0, 'CalPer' + age] != 0:
+                df_county.loc[i, 'Percent Deaths ' + age] = state.loc[0, 'CalPer' + age]
+            else:
+                df_county.loc[i, 'Percent Deaths ' + age] = df_national.loc[age, 'Percent Deaths']
 
 
+regional_groupings = rg.create_regional_hashmap()
 
-# Add columns for calculated deaths, calculated population, and calculated percent deaths in state table
-    df_state[['CalD0-5', 'CalD25+', 'CalD5-25', 'CalP0-5', 'CalP25+', 'CalP5-25', 'Per0-5', 'Per25+', 'Per5-25']] = [
-        np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
 
-    # Calculate death and population sums for each state from county data
-    # loop through all the state ids
+for age in ages:
+    df_county_sub_20 = df_county[(df_county['Deaths_' + age] < 20) & (df_county['Deaths_' + age] > 0)]
+    df_county_sub_20_regional = df_county_sub_20
     i = 0
     for i in range(df_state.shape[0]):
-        lookat = df_county[df_county['State ANSI'] == df_state.iloc[i, 0]]  # extract a subtable for each state
-        # print(lookat)
-        j = 0
-        for j in range(3):
-            df_state.iloc[i, j + 7] = lookat.iloc[:,
-                                      j + 2].sum()  # sum the deaths for each county in a state, for each age range, and put that calculated sum in the state table
-        # extract only the counties in a state with suppressed death rates (done automatically in sum of deaths)
-        lookatP0_5 = lookat[lookat['Deaths_0-5'] == 0]
-        lookatP5_25 = lookat[lookat['Deaths_5-25'] == 0]
-        lookatP25 = lookat[lookat['Deaths_25+'] == 0]
-        # sum the populations for only those counties that have a supressed death, add that value to the state table
-        df_state.iloc[i, 10] = lookatP0_5['Population_0-5'].sum()
-        df_state.iloc[i, 11] = lookatP25['Population_25+'].sum()
-        df_state.iloc[i, 12] = lookatP5_25['Population_5-25'].sum()
-
-    # Calculate the percent deaths in a state for only those counties with suppressed death counts
+        state = df_county_sub_20_regional[df_county_sub_20_regional['State ANSI'] == df_state.iloc[i, 0]]
+        if state.loc[:, 'Deaths_' + age].sum() >= 20:
+            df_county_sub_20_regional = df_county_sub_20_regional[df_county_sub_20_regional['State ANSI'] != df_state.iloc[i, 0]]  # drop all counties with current state code
+    df_county_sub_20_state = df_county_sub_20.drop(df_county_sub_20_regional.index)
     i = 0
-    j = 0
-    for i in range(df_state.shape[0]):
-        for j in range(3):  # do this foreach age group
-            if df_state.iloc[i, j + 10] != 0:
-                df_state.iloc[i, j + 13] = (df_state.iloc[i, j + 1] - df_state.iloc[i, j + 7]) / df_state.iloc[
-                    i, j + 10]
-    return df_state, df_county
+    for i in range(df_county.shape[0]):  # loop through all counties
+        if (df_county.loc[i, 'Deaths_' + age] < 20) & (df_county.loc[i, 'Deaths_' + age] > 0):  # if county death count is unreliable
+            if df_county.loc[i, 'State ANSI'] in list(df_county_sub_20_state['State ANSI']):  # if state code is in state table, use value in state table
+                current = df_county_sub_20_state[df_county_sub_20_state['State ANSI'] == df_county.loc[i, 'State ANSI']]
+                df_county.loc[i, 'Percent Deaths ' + age] = current.loc[:, 'Deaths_' + age].sum()/current.loc[:, 'Population_' + age].sum()  # replace death rate with sum of death rates in state table divided by population
+            else:
+                region = rg.get_region_from_state(df_county.loc[i, 'State ANSI'], regional_groupings)
+                current = df_county_sub_20_regional[df_county_sub_20_regional['State ANSI'].isin(regional_groupings[region])]
+                df_county.loc[i, 'Percent Deaths ' + age] = current.loc[:, 'Deaths_' + age].sum() / current.loc[:, 'Population_' + age].sum()
 
 
-
-
-
+for age in ages:
+    i = 0
+    for i in range(df_county.shape[0]):
+        if df_county.loc[i, 'Deaths_' + age] == 0:
+            df_county.loc[i, 'Deaths_' + age] = df_county.loc[i, 'Population_' + age] * df_county.loc[i, 'Percent Deaths ' + age]
 
 
 '''
-# calculate percent deaths for each age group
-df_county['Percent Deaths 0-5'] = df_county['Deaths_0-5'] / df_county['Population_0-5']
-df_county['Percent Deaths 25+'] = df_county['Deaths_25+'] / df_county['Population_25+']
-df_county['Percent Deaths 5-25'] = df_county['Deaths_5-25'] / df_county['Population_5-25']
+# Group ages into specified ranges
+df_county = df_county.replace({'Age Group Code': {'1': '0-5', '1-4': '0-5', '5-9': '5-25', '10-14': '5-25',
+                                                  '15-19': '5-25', '20-24': '5-25', '25-34': '25+', '35-44': '25+',
+                                                  '45-54': '25+', '55-64': '25+', '65-74': '25+', '75-84': '25+',
+                                                  '85+': '25+'}})
+df_state = df_state.replace({'Age Group Code': {'1': '0-5', '1-4': '0-5', '5-9': '5-25', '10-14': '5-25',
+                                                '15-19': '5-25', '20-24': '5-25', '25-34': '25+', '35-44': '25+',
+                                                '45-54': '25+', '55-64': '25+', '65-74': '25+', '75-84': '25+',
+                                                '85+': '25+'}})
+df_national = df_national.replace({'Age Group Code': {'1': '0-5', '1-4': '0-5', '5-9': '5-25', '10-14': '5-25',
+                                                      '15-19': '5-25', '20-24': '5-25', '25-34': '25+',
+                                                      '35-44': '25+', '45-54': '25+', '55-64': '25+',
+                                                      '65-74': '25+', '75-84': '25+', '85+': '25+'}})
 
-# Calculate the national death rate
-df_national['Percent Deaths'] = df_national['Deaths'] / df_national['Population']
+
+df_county = diff.fix(df_county, 1, 1, 1) # fill in missing counties
+df_county_no_estimates = df_county.round(5) # round data
+df_county_no_estimates.to_csv(r'Parsed data/All Cause Mortality.csv', index = False) #export without estimates
 '''
+
+df_county_test = df_county.round(5) # round data
+df_county_test.to_csv(r'Parsed data/All Cause Mortality.csv', index = False) #export without estimates
